0d7f75a3ea000a532507c1fffc0f7d5d778b3348
Mariana Souza Porto <160690963+Maryporto@users.noreply.github.com>
2024-04-28 22:47:24 -0300

    Oculos.java

---
+++ Oculos.java
@@ -0,0 +1,30 @@
+public class Oculos {
+    public String modelo;
+    public String durabilidade;
+  public String material;
+    public int grau;
+    
+    public Mochila(String modelo, String durabilidade, String material, int grau){
+        this.modelo.modelo;
+        this.durabilidade.durabilidade;
+      this.material=material;
+        this.grau.grau;
+    }public String getModelo(){
+        return modelo;
+    }public void setModelo(String modelo){
+        this.modelo=modelo;
+    }public String getDurabilidade(){
+        return durabilidade;
+    }public void setDurabilidade(String durabilidade){
+        this.durabilidade;
+    }public int getGrau(){
+        return grau;
+    }public void setGrau(int  grau){        
+        this.grau=grau;
+    } 
+    public String colocar(){
+        return "enxergar em full hd .";
+    }public String tirar(){
+        return "nao enxergar nada.";
+    } 
+}
